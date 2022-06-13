3625b695eb90415012fb9259f0ab8eb7030cc98b
Sammy <yeungsammy2002@yahoo.com.hk>
2022-06-13 13:28:24 +0800

    -

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const INIT_BAL = 8888;
 const MINING_REWARD = 88;
-const DIFFICULTY = 4;
+const DIFFICULTY = 6;
 const BLOCK_TIME = 4000;
+const BLOCK_TIME_TOLERANCE = 500;
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 module.exports = { INIT_BAL, MINING_REWARD, DIFFICULTY, BLOCK_TIME, HTTP_PORT };
